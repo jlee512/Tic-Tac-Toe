a6c49add2c7dbe960db15e40c9965968ff0de647
jlee512 <jlee512@aucklanduni.ac.nz>
2017-05-22 22:43:28 +1200

    Computer AI under development initial commit

---
+++ src/ComputerPlayer.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Julian on 21/05/2017.
+ */
+public class ComputerPlayer {
+
+    /*Implement minimax function as computer AI*/
+
+    ComputerPlayer(GameBoard game){
+
+    }
+
+
+
+}
